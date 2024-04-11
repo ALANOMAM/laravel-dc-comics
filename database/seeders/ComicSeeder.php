d57f9386a62bc32d0c4428cae80c0f80a39fb950
alan_omam <omamalan@hotmail.it>
2024-04-11 16:32:15 +0200

    aggiunto il model

--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -12,6 +12,26 @@ class ComicSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        /*$comics = config("comics");
+
+      / possiamo lasciarci dei messaggi in console con questo comando:
+        // questo comando ci lascia il messaggio solo quando il file in cui è scritto viene eseguito
+        // infatti in questo caso dobbiamo lanciare da terminale il comando per fare il seed, altrimenti non lo leggeremmo
+        // $this->command->info(print_r($pastas));
+
+
+        foreach($comics as $comic) {
+            $newComic = new Comic();
+
+            $newComic->src = $comic['src'];
+            $newComic->title = $comic['titolo'];
+            $newComic->description = $comic['descrizione'];
+            $newComic->type = $comic['tipo'];
+            $newComic->cooking_time = $comic['cottura'];
+            $newComic->weight = $comic['peso'];
+
+            $newComic->save();
+
+        } */
     }
 }
