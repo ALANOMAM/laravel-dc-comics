2f8c6b847ba71c2f5f68ca9adbd5e166fb76d3b1
alan_omam <omamalan@hotmail.it>
2024-04-16 11:58:00 +0200

    applicato il soft delete

--- app/Models/Comic.php
+++ app/Models/Comic.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+//il pezzo sotto delle soft deletes trasporta con se questo percorso
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Comic extends Model
 {
     use HasFactory;
+
+    //pezzo aggiunto apposta per le soft deletes 
+    use SoftDeletes;
 }
